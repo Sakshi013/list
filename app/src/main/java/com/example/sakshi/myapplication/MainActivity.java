fe900df6c2baec6e6ee7faf086ab3087da68588e
sakshi013 <sakshibhat013@gmail.com>
2018-06-18 21:26:48 +0530

    list

---
+++ app/src/main/java/com/example/sakshi/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sakshi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ListView listView =findViewById(R.id.shops);
+//        String[] shops = getResources().getStringArray(R.array.shops)
+        ListView listView = findViewById(R.id.shops);
+
+        final String[] Shops =getResources().getStringArray(R.array.Shops);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this,android.R.layout.simple_list_item_1,Shops);
+
+        listView.setAdapter(adapter);
+//   readClick
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(  MainActivity.this,"shops"+ Shops[position],Toast.LENGTH_SHORT ).show();
+
+            }
+        });
+    }
+}
